f4f593bf6e0858f91a3b6a1ae58876a749839c3e
Ana Geguchadze <anageguchadze22@gmail.com>
2025-01-06 14:46:18 +0100

    Addad CRUD functionally

--- crud.py
+++ crud.py
@@ -19,13 +19,40 @@ class Student(db.Model):
 with app.app_context():
     db.create_all()
 
-@app.route("/student", methods=['POST'])
+@app.route("/students", methods=['POST'])
 def create_students():
     data = request.get_json()
     new_student = Student(name=data['name'], subject=data["subject"])
     db.session.add(new_student)
     db.session.commit()
     return jsonify({"message": "Student was created!"}), 201
+
+@app.route('/students', methods=['GET'])
+def get_students():
+    students = Student.query.all()
+    return jsonify([{"id": student.id, "name": student.name, "subject": student.subject} for student in students])
+
+
+@app.route('/students/<int:id>', methods=['PUT'])
+def update_student(id):
+    data = request.get_json()
+    student = Student.query.get(id)
+    if not student:
+        return jsonify({"message": "Student not found"}), 404
+    student.name = data['name']
+    student.subject = data['subject']
+    db.session.commit()
+    return jsonify({"message": "Student updated successfully"})
+
+
+@app.route('/students/<int:id>', methods=['DELETE'])
+def delete_student(id):
+    student = Student.query.get(id)
+    if not student:
+        return jsonify({"message": "Student not found"}), 404
+    db.session.delete(student)
+    db.session.commit()
+    return jsonify({"message": "Student deleted successfully"})
 
 
 if __name__=="__main__":
